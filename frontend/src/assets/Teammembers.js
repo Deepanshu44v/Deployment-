96650039775d191825e30a67b400b4843bd09fb0
Deepanshu Singh <92912638+Deepanshu44v@users.noreply.github.com>
2024-08-03 13:38:20 +0530

    Update Teammembers.js

--- frontend/src/assets/Teammembers.js
+++ frontend/src/assets/Teammembers.js
@@ -4,10 +4,10 @@ import mintuImage from './mintuphoto.jpeg'
 import kanishkaImage from './kanishkaphoto.jpg'
 import ayanImage from './ayanphoto.jpg'
 const teamMembers = [
-    { name: 'Deepanshu Singh', role: 'Lead Developer', imageUrl: sahilImage },
-    { name: 'Mintu Chaudhary', role: 'CEO, Marketing Head', imageUrl: mintuImage },
-    { name: 'Kanishka', role: 'Lead Designer', imageUrl: kanishkaImage },
-    { name: 'Ayan Firdosh', role: 'Project Manager', imageUrl: ayanImage },
+    { name: 'Deepanshu Singh', role: 'Developer', imageUrl: sahilImage },
+    { name: 'Mintu Chaudhary', role: 'Project Head', imageUrl: mintuImage },
+    // { name: 'Kanishka', role: 'Lead Designer', imageUrl: kanishkaImage },
+    { name: 'Ayan Firdosh', role: 'Developer', imageUrl: ayanImage },
 ];
 export default teamMembers;
 // this is for our team detail and is we have to add a member to our team we can add data here and team is automatically added to our about page page no need to hardcode there
